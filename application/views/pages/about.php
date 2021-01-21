b5c34b5fac70cd0fb08bb094f5b78651a898e5f2
Lynette's <lynettes@Ochiengs-iMac.local>
2021-01-21 12:33:16 +0300

    initial commit

---
+++ application/views/pages/about.php
@@ -0,0 +1,2 @@
+<h2><?= $title ?></h2>
+<p>This is the ciBlog static version</p>
---
+++ application/views/templates/header.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+        <title>ciBlog</title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/flatly/bootstrap.min.css">
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
+                </div>
+                <div id="navbar">
+                    <ul class="navbar-nav mr-auto">
+                        <li><a href="<?php echo base_url(); ?>">Home</a></li>
+                        <li><a href="<?php echo base_url(); ?>/about">About</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <div class="container">
