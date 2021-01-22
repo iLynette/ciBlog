909cfea1dbb167e2d4e673ab8ba2cad508dd5b2c
Lynette's <lynettes@Ochiengs-iMac.local>
2021-01-22 12:13:29 +0300

    creating posts

--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -3,6 +3,6 @@
     <h3><?php echo $post['title']; ?></h3>
     <small class="post-date">Posted on: <?php echo $post['created_at']; ?></small><br>
     <?php echo $post['body']; ?>
-    <br><br><br>
-    <p><a class="btn btn-default" href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More</a></p>
+    <br><br>
+    <p><a class="btn btn-secondary" href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More</a></p>
 <?php }?>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -2,7 +2,7 @@
     <head>
         <title>ciBlog</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootswatch/4.5.2/flatly/bootstrap.min.css">
-        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css"
+        <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css">
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
@@ -11,7 +11,7 @@
                     <a class="navbar-brand" href="<?php echo base_url(); ?>">ciBlog</a>
                 </div>
                 <div id="navbar">
-                    <ul class="navbar-nav mr-auto">
+                    <ul class="navbar-nav">
                         <li><a href="<?php echo base_url(); ?>">Home</a></li>
                         <li><a href="<?php echo base_url(); ?>about">About</a></li>
                         <li><a href="<?php echo base_url(); ?>posts">Blog</a></li>
