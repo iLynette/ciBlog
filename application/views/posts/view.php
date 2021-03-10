28ce793258909eadef07dc12f5a4d34c874e239d
Lynette <achol>
2021-03-10 12:23:54 +0300

    comment functionality

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -10,13 +10,25 @@
     <input type="submit" value="Delete" class="btn btn-danger">
 </form>
 <hr>
+<h3>Comments</h3>
+<?php if($comments) : ?>
+    <?php foreach($comments as $comment) : ?>
+    <div class="well">
+        <h5><?php echo $comment['body']; ?> [by <strong><?php echo $comment['name']; ?></strong>]</h5>
+    </div>
+    <?php endforeach; ?>
+    
+    <?php else : ?>
+        <p>No comments to display</p>
+    <?php endif; ?>
+<hr>
 <h3>Add comment</h3>
 <?php echo  validation_errors(); ?>
 <?php echo form_open('comments/create/'.$post['id']); ?>
     <div class="form-group">
         <label>Name</label>
         <input type="text" name="name" class="form-control">
-    </div>
+    </div>  
     <div class="form-group">
         <label>Email</label>
         <input type="text" name="email" class="form-control">
